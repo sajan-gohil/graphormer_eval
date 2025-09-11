978ba7ad89bfb10c4b2016257a4cfae5622e77f3
sajan-gohil <sajangohil9@gmail.com>
2025-09-11 07:23:10 +0000

    added config in diffusion init

--- graphormer_hf/modeling_graphormer.py
+++ graphormer_hf/modeling_graphormer.py
@@ -366,7 +366,7 @@ class GraphormerMultiheadAttention(nn.Module):
         )
 
         self.onnx_trace = False
-        self.diffusion_model = GraphLatentDiffusion(self.kdim, config.embedding_dim, config.diffusion_steps)
+        self.diffusion_model = GraphLatentDiffusion(self.kdim, config.embedding_dim, config.diffusion_steps, config=self.config)
 
     def reset_parameters(self):
         if self.qkv_same_dim:
