fixed feature normalization position

graphormer_hf/modeling_graphormer.py
@@ -85,44 +85,6 @@ def compute_attention_snr(attn_weights, labels, node_mask):
     return snr
 
 
-class LayerDropModuleList(nn.ModuleList):
-    """
-    From:
-    https://github.com/facebookresearch/fairseq/blob/dd0079bde7f678b0cd0715cbd0ae68d661b7226d/fairseq/modules/layer_drop.py
-    A LayerDrop implementation based on [`torch.nn.ModuleList`]. LayerDrop as described in
-    https://huggingface.co/papers/1909.11556.
-
-    We refresh the choice of which layers to drop every time we iterate over the LayerDropModuleList instance. During
-    evaluation we always iterate over all layers.
-
-    Usage:
-
-    ```python
-    layers = LayerDropList(p=0.5, modules=[layer1, layer2, layer3])
-    for layer in layers:  # this might iterate over layers 1 and 3
-        x = layer(x)
-    for layer in layers:  # this might iterate over all layers
-        x = layer(x)
-    for layer in layers:  # this might not iterate over any layers
-        x = layer(x)
-    ```
-
-    Args:
-        p (float): probability of dropping out each layer
-        modules (iterable, optional): an iterable of modules to add
-    """
-
-    def __init__(self, p: float, modules: Optional[Iterable[nn.Module]] = None):
-        super().__init__(modules)
-        self.p = p
-
-    def __iter__(self) -> Iterator[nn.Module]:
-        dropout_probs = torch.empty(len(self)).uniform_()
-        for i, m in enumerate(super().__iter__()):
-            if not self.training or (dropout_probs[i] > self.p):
-                yield m
-
-
 class GraphormerGraphNodeFeature(nn.Module):
     """
     Compute node features for each node in the graph.
@@ -134,14 +96,14 @@ class GraphormerGraphNodeFeature(nn.Module):
         self.num_heads = config.num_attention_heads
         self.num_atoms = config.num_atoms
 
-        self.atom_encoder = nn.Embedding(config.num_atoms + 1, config.hidden_size, padding_idx=config.pad_token_id)
+        # self.atom_encoder = nn.Embedding(config.num_atoms + 1, config.hidden_size, padding_idx=config.pad_token_id)
         self.feature_encoder = nn.Linear(1433, config.hidden_size)
-        self.in_degree_encoder = nn.Embedding(
-            config.num_in_degree, config.hidden_size, padding_idx=config.pad_token_id
-        )
-        self.out_degree_encoder = nn.Embedding(
-            config.num_out_degree, config.hidden_size, padding_idx=config.pad_token_id
-        )
+        # self.in_degree_encoder = nn.Embedding(
+        #     config.num_in_degree, config.hidden_size, padding_idx=config.pad_token_id
+        # )
+        # self.out_degree_encoder = nn.Embedding(
+        #     config.num_out_degree, config.hidden_size, padding_idx=config.pad_token_id
+        # )
 
         self.graph_token = nn.Embedding(1, config.hidden_size)  # Equivalent to [CLS]
 
@@ -154,16 +116,16 @@ class GraphormerGraphNodeFeature(nn.Module):
         n_graph, n_node = input_nodes.size()[:2]
         # print("Input nodes:", input_nodes.shape)
         if self.config.dataset_name not in ["pcqm4mv2"]:
+            input_nodes = input_nodes.to(dtype=torch.float32)
+            input_nodes = input_nodes / (input_nodes.norm(dim=-1, keepdim=True) + 1e-8)  # l2 norm
             node_feature = (
                self.feature_encoder(input_nodes.to(dtype=torch.float32))  # nn.functional.relu(self.feature_encoder(input_nodes.to(dtype=torch.float32))))
-               + self.in_degree_encoder(in_degree)
-               + self.out_degree_encoder(out_degree)
+               # + self.in_degree_encoder(in_degree)
+               # + self.out_degree_encoder(out_degree)
             )
             # print("Processed")
         else:
             # Normalize each node by l2 norm of the node features
-            input_nodes = input_nodes.to(dtype=torch.float32)
-            input_nodes = input_nodes / (input_nodes.norm(dim=-1, keepdim=True) + 1e-8)  # l2 norm
             node_feature = (  # node feature + graph token
                 self.atom_encoder(input_nodes).sum(dim=-2)  # [n_graph, n_node, n_hidden]
                 # + self.in_degree_encoder(in_degree)